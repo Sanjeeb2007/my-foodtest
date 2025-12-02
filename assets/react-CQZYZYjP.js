58d46ce74a48aa9f2cbb69c5bf837f16af75628b
Sanju nepali <sanju@gmail.com>
2025-12-02 14:12:38 +0530

    Updates

--- assets/react-CQZYZYjP.js
+++ assets/react-CQZYZYjP.js
@@ -1,4 +1,4 @@
-import{n as e,r as t}from"./index-DPT6zz9T.js";import{t as n}from"./hls-flxay8fq.js";import{n as r,t as i}from"./dist-B7fTPFV_.js";var a=t(e(),1),o=(e=>class extends e{static shadowRootOptions={...e.shadowRootOptions};static getTemplateHTML=(t,n={})=>{let{src:r,...i}=t;return`
+import{n as e,r as t}from"./index-CIVa5v5W.js";import{t as n}from"./hls-flxay8fq.js";import{n as r,t as i}from"./dist-B7fTPFV_.js";var a=t(e(),1),o=(e=>class extends e{static shadowRootOptions={...e.shadowRootOptions};static getTemplateHTML=(t,n={})=>{let{src:r,...i}=t;return`
         <script type="application/json" id="config">
           ${JSON.stringify(n.config||{})}
         <\/script>
